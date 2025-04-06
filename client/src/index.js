b06df7fbcffef753553252b73be49dc24e18d64b
Peter Leonov <gojpeg@gmail.com>
2025-04-06 19:08:41 +0200

    type check client

--- client/src/index.js
+++ client/src/index.js
@@ -3,23 +3,46 @@ const socket = new WebSocket("ws://localhost:8787/tunnel");
 
 // Executes when the connection is successfully established.
 socket.addEventListener("open", (event) => {
-  console.log("WebSocket connection established!");
+  console.log("Connected.", event);
   // Sends a message to the WebSocket server.
-  socket.send("Hello Server!");
 });
 
-// Listen for messages and executes when a message is received from the server.
-socket.addEventListener("message", (event) => {
-  console.log("Message from server: ", event.data);
+socket.addEventListener("message", async (event) => {
+  console.debug("event.data", event.data);
+  const message = JSON.parse(event.data);
+  if (message.type === "ping") {
+    socket.send(JSON.stringify({ type: "pong" }));
+  } else if (message.type === "request") {
+    // As the client code is the easies to test and debug, we will
+    // handle all the edge cases with transporting the request
+    // and response objects here (e.g. keep-alive, gzip, etc.)
+    console.debug("start fetch", event.data);
+    const response = await fetch("http://localhost:3000");
+    let body;
+    if (response.body) {
+      body = Buffer.from(await response.bytes()).toString("base64");
+    }
+
+    const responseSerializable = {
+      status: response.status,
+      statusText: response.statusText,
+      headers: [...response.headers.entries()],
+      body,
+    };
+    socket.send(
+      JSON.stringify({
+        type: "response",
+        response: responseSerializable,
+      })
+    );
+  } else {
+    console.error("Unknown message type:", message.type);
+  }
 });
 
 // Executes when the connection is closed, providing the close code and reason.
 socket.addEventListener("close", (event) => {
-  console.log(
-    "WebSocket connection closed:",
-    event.code,
-    event.reason
-  );
+  console.log("Connection closed:", event.code, event.reason);
 });
 
 // Executes if an error occurs during the WebSocket communication.
