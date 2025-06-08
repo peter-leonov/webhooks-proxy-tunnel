44c8c66a4c6e2f1a277dbf056eecd1f5be146120
Peter Leonov <gojpeg@gmail.com>
2025-06-08 11:56:14 +0200

    dump more data

--- server/src/index.js
+++ server/src/index.js
@@ -5,9 +5,21 @@ let counter = 0;
 const server = http.createServer((req, res) => {
   console.log(`${req.method} ${req.url}`);
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
+  res.setHeader("Content-Type", "text/html");
   counter++;
-  res.write(`Hello, World #${counter}!\n\n`);
+  res.write(`<p>Request #${counter}!</p><p>\n`);
+  res.write(`<p><pre><code>`);
+  res.write(`${req.method} ${req.url}\n`);
+  res.write(
+    `${Object.entries(req.headers)
+      .map(([key, value]) => `${key}: ${value}`)
+      .join("\n")}`
+  );
+  res.write(`</code></pre></p>\n`);
+  res.write(
+    `<form method="POST" action="."><input type="text" name="name" placeholder="Enter your name"><button type="submit">Submit</button></form></p>`
+  );
+  res.write(`Request body:<hr><p><pre><code>`);
   req.pipe(res, { end: true });
 });
 
